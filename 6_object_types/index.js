da19de05109fb7c224f8cca52ff39e879bb7fc7b
Valdo Neves <valdo@alfasense.com.br>
2023-05-17 18:13:08 -0300

    [MAIN] readonly

--- 6_object_types/index.js
+++ 6_object_types/index.js
@@ -35,3 +35,9 @@ const u1 = { email: "valdo@email.com", role: "Admin" };
 const u2 = { email: "joao@email.com" };
 showUserDetails(u1);
 showUserDetails(u2);
+const fusca = {
+    brand: "VW",
+    wheels: 4
+};
+console.log(fusca);
+// fusca.wheels = 34 // não aceita
