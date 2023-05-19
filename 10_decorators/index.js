a4df7f6cabcb28a03ed14889ebaf723e8d6dc114
Valdo Neves <valdo@alfasense.com.br>
2023-05-19 02:22:27 -0300

    [10-DECORATORS] property decorator
    formatando um valor via decorators

--- 10_decorators/index.js
+++ 10_decorators/index.js
@@ -22,7 +22,7 @@ class MyClass {
     }
 }
 __decorate([
-    myDecorator() // aparece esse erro mas compila ate a metade
+    myDecorator()
 ], MyClass.prototype, "teste", null);
 const myObj = new MyClass();
 myObj.teste();
@@ -50,3 +50,96 @@ __decorate([
 ], MultipleDecorators.prototype, "testing", null);
 const multiple = new MultipleDecorators();
 multiple.testing();
+// 3 - decorator de classe 
+function classDecorator(constructor) {
+    console.log(constructor); // pegando a class
+    if (constructor.name === "User") { // nome da classe
+        console.log("Criando usuário");
+    }
+}
+let User = class User {
+    constructor(name) {
+        this.name = name;
+    }
+};
+User = __decorate([
+    classDecorator
+], User);
+const personOne = new User("Eu");
+console.log(personOne);
+// 4 - method decorator
+function enumerable(value) {
+    return function (target, propertKey, descriptor) {
+        descriptor.enumerable = value;
+    };
+}
+class Machine {
+    constructor(name) {
+        this.name = name;
+    }
+    /**
+     * com false quando abrimos as propriedades no console do navegador vemos que o metodo showName esta apagado
+     * com true o metodo showName fica acesso
+     */
+    showName() {
+        console.log(this);
+        return `O nome da maquina é ${this.name}`;
+    }
+}
+__decorate([
+    enumerable(false)
+    /**
+     * com false quando abrimos as propriedades no console do navegador vemos que o metodo showName esta apagado
+     * com true o metodo showName fica acesso
+     */
+], Machine.prototype, "showName", null);
+const trator = new Machine("Trator");
+console.log(trator.showName());
+// 5 - acessor decorator (getter and setter)
+class Monster {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    get showName() {
+        return `O nome do monstro: ${this.name}`;
+    }
+    get showAge() {
+        return `A idade do monstro: ${this.age}`;
+    }
+}
+__decorate([
+    enumerable(true)
+], Monster.prototype, "showName", null);
+__decorate([
+    enumerable(true)
+], Monster.prototype, "showAge", null);
+const charmander = new Monster("Charmander", 10);
+console.log(charmander);
+// 6 - property decorator
+// criar um decorator para formar o meu id para 00001
+function formatNumber() {
+    return function (target, propertKey) {
+        let value;
+        const getter = function () {
+            return value;
+        };
+        const setter = function (newVal) {
+            value = newVal.padStart(5, "0");
+        };
+        Object.defineProperty(target, propertKey, {
+            set: setter,
+            get: getter
+        });
+    };
+}
+class ID {
+    constructor(id) {
+        this.id = id;
+    }
+}
+__decorate([
+    formatNumber()
+], ID.prototype, "id", void 0);
+const newItem = new ID("1");
+console.log(newItem);
--- 10_decorators/index.ts
+++ 10_decorators/index.ts
@@ -16,7 +16,7 @@ function myDecorator(){
 }
 class MyClass {
   
-  @myDecorator() // aparece esse erro mas compila ate a metade
+  @myDecorator()
   teste(){
     console.log("Terminando execução do método!")
   }
@@ -59,3 +59,111 @@ class MultipleDecorators{
 const multiple = new MultipleDecorators()
 
 multiple.testing()
+
+// 3 - decorator de classe 
+function classDecorator(constructor:Function){
+  console.log(constructor) // pegando a class
+  if(constructor.name === "User"){ // nome da classe
+    console.log("Criando usuário")
+  }
+}
+
+@classDecorator
+class User {
+  name
+
+  constructor(name:string){
+    this.name = name
+  }
+}
+
+const personOne = new User("Eu")
+
+console.log(personOne)
+
+// 4 - method decorator
+function enumerable(value:boolean){
+  return function(target:any, propertKey:string, descriptor:PropertyDescriptor){
+    descriptor.enumerable = value
+  }
+}
+class Machine {
+  name
+
+  constructor(name:string){
+    this.name = name
+  }
+
+  @enumerable(false) 
+  /**
+   * com false quando abrimos as propriedades no console do navegador vemos que o metodo showName esta apagado
+   * com true o metodo showName fica acesso
+   */
+  showName(){
+    console.log(this)
+    return `O nome da maquina é ${this.name}`
+  }
+}
+
+const trator = new Machine("Trator")
+
+console.log(trator.showName())
+
+// 5 - acessor decorator (getter and setter)
+class Monster {
+  name?
+  age?
+
+  constructor(name:string, age:number){
+    this.name = name
+    this.age = age
+  }
+
+  @enumerable(true)
+  get showName(){
+    return `O nome do monstro: ${this.name}`
+  }
+
+  @enumerable(true)
+  get showAge(){
+    return `A idade do monstro: ${this.age}`
+  }
+}
+
+const charmander = new Monster("Charmander", 10)
+
+console.log(charmander)
+
+// 6 - property decorator
+// criar um decorator para formar o meu id para 00001
+function formatNumber(){
+  return function(target: Object, propertKey: string){
+    let value:string
+
+    const getter = function(){
+      return value
+    }
+
+    const setter = function(newVal: string){
+      value = newVal.padStart(5, "0")
+    }
+
+    Object.defineProperty(target, propertKey, {
+      set: setter,
+      get: getter
+    })
+
+  }
+}
+class ID {
+  @formatNumber()
+  id
+
+  constructor(id:string){
+    this.id = id
+  }
+}
+
+const newItem = new ID("1")
+
+console.log(newItem)
