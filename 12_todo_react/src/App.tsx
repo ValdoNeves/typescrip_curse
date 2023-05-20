e53675d13a5d299a8b3a09fd025b1092d46c266d
Valdo Neves <valdo@alfasense.com.br>
2023-05-20 17:48:22 -0300

    [12] Estilização inicial do projeto

---
+++ 12_todo_react/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main className={styles.main}></main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
