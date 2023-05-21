828ce15cb08bb36dc210dd97ed1d4e6923399e65
Valdo Neves <valdo@alfasense.com.br>
2023-05-21 19:31:40 -0300

    [14] mensagem generica para o usuario com status 500

--- 14_api_restful_com_ts/src/controllers/movieControllers.ts
+++ 14_api_restful_com_ts/src/controllers/movieControllers.ts
@@ -6,36 +6,57 @@ import { MovieModel } from "../models/Movie";
 //Logger
 import Logger from "../../config/logger";
 
-export async function createMovie(req:Request, res:Response){
-  
+export async function createMovie(req: Request, res: Response) {
+
   try {
     const data = req.body
     const movie = await MovieModel.create(data)
     res.status(201).json(movie)
-    
-  } catch (e:any) {
+
+  } catch (e: any) {
     Logger.error(`Erro no sistema: ${e.message}`)
-    res.status(500).send(e)
+    res.status(500).send({error: "Por Favor, tente novamente mais tarde!"})
 
   }
 }
 
-export async function findMovieById(req:Request,res:Response) {
-  
-  try{
-    
+export async function findMovieById(req: Request, res: Response) {
+
+  try {
+
     const id = req.params.id
     const movie = await MovieModel.findById(id);
 
-    if(!movie){
-      return res.status(404).json({error: "Filme não encontrado!"})
+    if (!movie) {
+      return res.status(404).json({ error: "Filme não encontrado!" })
     }
 
     return res.status(200).json(movie);
 
-  } catch (e:any) {
+  } catch (e: any) {
     Logger.error(`Erro no sistema: ${e.message}`)
-    res.status(500).send(e)
+    res.status(500).send({error: "Por Favor, tente novamente mais tarde!"})
+  }
+}
 
+export async function getAllMovies(req: Request, res: Response) {
+  try {
+
+    const movies = await MovieModel.find();
+    return res.status(200).json(movies)
+
+  } catch (e: any) {
+    Logger.error(`Erro no sistema: ${e.message}`)
+    res.status(500).send({error: "Por Favor, tente novamente mais tarde!"})
+  }
+}
+
+export async function removeMovies(req: Request, res: Response) {
+  try {
+
+
+  } catch (e: any) {
+    Logger.error(`Erro no sistema: ${e.message}`)
+    res.status(500).send({error: "Por Favor, tente novamente mais tarde!"})
   }
 }
