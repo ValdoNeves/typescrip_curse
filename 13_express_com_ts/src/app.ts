2dc3a8bbca6ba6f54967594c3bcb4f5eccefdbad
Valdo Neves <valdo@alfasense.com.br>
2023-05-21 00:24:48 -0300

    [13-NODE] Rodas com 1 ou mais parametros

--- 13_express_com_ts/src/app.ts
+++ 13_express_com_ts/src/app.ts
@@ -2,7 +2,7 @@
 // console.log("Express + TS")
 
 // 2 - init express
-import  express from "express";
+import  express, {Request, Response} from "express";
 
 const app = express();
 
@@ -33,6 +33,58 @@ app.all("/api/product/check", (req, res) => {
     return res.send("não podemos realizar essa operação!")
   }
 })
+
+// 5 - interface do express
+app.get("/api/interfaces", (req:Request, res: Response) => {
+  return res.send("utilizando as interfaces!")
+})
+
+// 6 - enviando json
+app.get("/api/json", (req:Request, res:Response) => {
+
+  return res.json({
+    "nome": "Shirt",
+    "price": 30,
+    "color": "white",
+    "size": ["P", "M", "G"]
+
+  })
+
+})
+
+// 7 - router parameters
+app.get("/api/product/:id", (req:Request, res: Response) => {
+
+  console.log(req.params);
+
+  const id = req.params.id
+
+  if(id === "1"){
+    const product = {
+      id: 1,
+      name: "boné",
+      price: 10,
+    }
+    res.json(product)
+  }else{
+    res.send("produto não encontrado 404")
+  }
+})
+
+// 8 - rodas complexas 
+app.get("/api/product/:id/review/:reviewId", (req:Request, res: Response) => {
+  console.log(req.params)
+
+  const productId = req.params.id
+  const reviewId = req.params.reviewId
+
+
+
+  res.send(`Acessando a review ${reviewId} do produto ${productId}`)
+
+})
+
+
 app.listen(3000, () => {
   console.log("Aplicação de TS + Express funcionando!");
 })
