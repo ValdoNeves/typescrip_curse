2d683c84ce806d028faf0718c036163deb1c1084
Valdo Neves <valdo@alfasense.com.br>
2023-05-21 13:00:16 -0300

    [14] Configurações globais
    .env sendo criado na raiz do projeto mas não sendo gerenciado pelo git
    por conter dados sensiveis, assim a aplicação fica um pouco mais segura
    
    modificação na forma de instanciar o banco de dados devido os dados
    globais

--- 14_api_restful_com_ts/config/default.ts
+++ 14_api_restful_com_ts/config/default.ts
@@ -1,4 +1,7 @@
+const dbUser = process.env.DB_USER
+const dbPsword = process.env.DB_PSWORD
+
 export default {
   port: 3000,
-  dbUri: "mongodb+srv://valdo_neves:valdo135@cluster0.jkghsio.mongodb.net/?retryWrites=true&w=majority"
+  dbUri: `mongodb+srv://${dbUser}:${dbPsword}@cluster0.jkghsio.mongodb.net/?retryWrites=true&w=majority`
 }
