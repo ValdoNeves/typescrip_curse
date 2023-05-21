12613472a3828d5bfbef8a099208dfd174571f46
Valdo Neves <valdo@alfasense.com.br>
2023-05-21 20:08:09 -0300

    [14] Finalizando estruturação e tratamento do backend com node
    utilizando ferramentas de log, validação, banco de dado em nuvem, e
    gerando uma API restful

--- 14_api_restful_com_ts/src/controllers/movieControllers.ts
+++ 14_api_restful_com_ts/src/controllers/movieControllers.ts
@@ -15,7 +15,7 @@ export async function createMovie(req: Request, res: Response) {
 
   } catch (e: any) {
     Logger.error(`Erro no sistema: ${e.message}`)
-    res.status(500).send({error: "Por Favor, tente novamente mais tarde!"})
+    res.status(500).json({ error: "Por Favor, tente novamente mais tarde!" })
 
   }
 }
@@ -35,7 +35,7 @@ export async function findMovieById(req: Request, res: Response) {
 
   } catch (e: any) {
     Logger.error(`Erro no sistema: ${e.message}`)
-    res.status(500).send({error: "Por Favor, tente novamente mais tarde!"})
+    res.status(500).json({ error: "Por Favor, tente novamente mais tarde!" })
   }
 }
 
@@ -47,16 +47,47 @@ export async function getAllMovies(req: Request, res: Response) {
 
   } catch (e: any) {
     Logger.error(`Erro no sistema: ${e.message}`)
-    res.status(500).send({error: "Por Favor, tente novamente mais tarde!"})
+    res.status(500).json({ error: "Por Favor, tente novamente mais tarde!" })
   }
 }
 
 export async function removeMovies(req: Request, res: Response) {
   try {
 
+    const id = req.params.id
+    const movie = await MovieModel.findById(id);
+
+    if (!movie) {
+      return res.status(404).json({ error: "Filme não encontrado!" })
+    }
+
+    await movie.deleteOne()
+
+    res.status(200).json({ mgs: "filme removido com sucesso!" })
+  } catch (e: any) {
+    Logger.error(`Erro no sistema: ${e.message}`)
+    res.status(500).json({ error: "Por Favor, tente novamente mais tarde!" })
+  }
+}
+
+export async function updateMovie(req: Request, res: Response) {
+  try {
+
+    const id =  req.params.id
+    const data = await req.body
+    const movie = await MovieModel.findById(id);
+
+    if (!movie) {
+      return res.status(404).json({ error: "Filme não encontrado!" })
+    }else{
+      //encontra o Id e atualiza os dados
+      await MovieModel.updateOne({_id:id}, data)
+  
+      return res.status(200).json(movie)
+    }
 
   } catch (e: any) {
     Logger.error(`Erro no sistema: ${e.message}`)
-    res.status(500).send({error: "Por Favor, tente novamente mais tarde!"})
+    res.status(500).json({ error: "Por Favor, tente novamente mais tarde!" })
   }
 }
--- 14_api_restful_com_ts/src/router.ts
+++ 14_api_restful_com_ts/src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express'
-import { createMovie, findMovieById, getAllMovies } from './controllers/movieControllers';
+import { createMovie, findMovieById, getAllMovies, removeMovies, updateMovie } from './controllers/movieControllers';
 
 //validations
 import { validate } from './middleware/handleValidation'
@@ -14,3 +14,5 @@ export default router
   .post("/movie", movieCreateValidation(), validate, createMovie)
   .get("/movie/:id", findMovieById)
   .get("/movie", getAllMovies)
+  .delete("/movie/:id", removeMovies)
+  .patch("/movie/:id", movieCreateValidation(), validate, updateMovie)
